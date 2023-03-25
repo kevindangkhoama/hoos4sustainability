c214db29624b7c254193125d9095f7d28951efd2
Tiffany Bui <tiffanybui@MacBook-Pro-3.local>
2023-03-25 14:44:20 -0400

    working!!

--- CS3240F18/urls.py
+++ CS3240F18/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('textbook/', include('textbookexchange.urls')),
+    path('sustainability/', include('textbookexchange.urls')),
     path('admin/', admin.site.urls),
 ]
