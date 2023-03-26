2fd3b9eae4137079247a688b9f8fc600d5774609
Tiffany Bui <tiffanybui@MacBook-Pro-3.local>
2023-03-25 22:01:03 -0400

    logout is working!!!!

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
          name='login'),
     path('ship/', auth_views.LoginView.as_view(template_name='hello/ship.html'), name='ship'),
     path('points/', auth_views.LoginView.as_view(template_name='hello/points.html'), name='points'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='hello:home')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='hello/hoos.html')),
     path('register/', RegisterView.as_view(), name='register'),
     path('', include('django.contrib.auth.urls'))
 ]
