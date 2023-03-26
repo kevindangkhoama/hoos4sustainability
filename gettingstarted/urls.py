be449eadef4355ba1514d1fc1d51312716e7082f
Tiffany Bui <tiffanybui@MacBook-Pro-3.local>
2023-03-25 21:34:03 -0400

    im almost happy

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,6 +5,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
+from django.urls import path
+from django.contrib import auth
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+from hello.views.register import RegisterView
+from hello.views.comment import CommentCreate
+from hello.views.home import home
+from hello.views.post import PostView, PostCreate, PostUpdate, PostDelete
 
 # To add a new path, first import the app:
 # import blog
@@ -14,8 +24,21 @@ import hello.views
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
+# urlpatterns = [
+#     path("", hello.views.index, name="index"),
+#     path("db/", hello.views.db, name="db"),
+#     path("admin/", admin.site.urls),
+# ]
+
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
-    path("admin/", admin.site.urls),
+    path('hoos/', auth_views.LoginView.as_view(template_name='hello/hoos.html'), name='hoos'),
+    path('admin/', admin.site.urls),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='hello/login.html'),
+         name='login'),
+    path('ship/', auth_views.LoginView.as_view(template_name='hello/ship.html'), name='ship'),
+    path('points/', auth_views.LoginView.as_view(template_name='hello/points.html'), name='points'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='hello:home')),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls'))
 ]
---
+++ hello/templates/hello/action_get.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Welcome <?php echo $_GET["Start"]; ?><br>
+Your email address is: <?php echo $_GET["Destination"]; ?>
+</body>
+</html>
